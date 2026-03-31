23adba40e9d6a44513b97547e2ebbcc8981ec71f
Emerson Creator <emerson.albornoz@correounivalle.edu.co>
2026-03-31 11:52:12 -0500

    Added first custom js action

---
+++ .github/actions/deploy-s3-javascript/index.js
@@ -0,0 +1,9 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+
+function run() {
+  core.info("Deploying to S3 (JavaScript)...");
+}
+
+run();

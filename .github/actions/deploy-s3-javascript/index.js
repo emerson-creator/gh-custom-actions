3a109940082195d6af207d2f4ba942b9540bb9d8
Emerson Creator <emerson.albornoz@correounivalle.edu.co>
2026-03-31 16:27:59 -0500

    Added custom deploy action

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,6 +3,17 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  // 1) Get some inputs values
+  const bucketName = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // 2) Upload files
+
+  exec.exec(
+    `aws s3 sync ${distFolder} s3://${bucketName} --region ${bucketRegion} --delete`,
+  );
+
   core.info("Deploying to S3 (JavaScript)...");
 }
 
